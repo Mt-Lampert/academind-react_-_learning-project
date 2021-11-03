c5a3847fc40cba00f0e9388958835ac2bf1f1fdc
Mt-Lampert <perrin.blog@web.de>
2021-11-03 16:21:28 +0100

    w: Outlining UserList Component

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,9 +1,23 @@
 import "./UserList.css";
 
 const UserList = (props) => {
+  const userList = [
+    { name: "Rich Harris", age: "37" },
+    { name: "Laura Bitchface", age: "31" },
+    { name: "Mark Wilson", age: "25" },
+  ];
+
   return (
-    <div className="user-list">
-      <h2>Under Construction: User List</h2>
+    <div className="user-container">
+      <div className="user-list">
+        {userList.map((user) => {
+          return (
+            <div className="user-entry">
+              {user.name}, {user.age} years old.
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
