d3a8b1637ac0a15fcadc9146e790d442762ee989
Mt-Lampert <perrin.blog@web.de>
2021-11-03 12:17:58 +0100

    w: adding basic components

---
+++ src/components/UserList.js
@@ -0,0 +1,11 @@
+import "./UserList.css";
+
+const UserList = (props) => {
+  return (
+    <div className="user-list">
+      <h2>Under Construction: User List</h2>
+    </div>
+  );
+};
+
+export default UserList;
